73f21ad96ed7c63cbe0b3e10f64690b76832285f
Anton Yabchinskiy <arn@bestmx.net>
2016-02-12 14:19:52 +0300

    web: add sup and app modules

---
+++ src/wrked_app.erl
@@ -0,0 +1,25 @@
+-module(wrked_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%%====================================================================
+%% Application callbacks
+%%====================================================================
+
+start(_StartType, _StartArgs) ->
+    Paths = [ ],
+    Host = {'_', Paths},
+    Dispatch = cowboy_router:compile([Host]),
+    Addr = application:get_env(wrked, addr, {127,0,0,1}),
+    Port = application:get_env(wrked, port, 8080),
+    {ok, _Pid} =
+        cowboy:start_http(wrked_http, 100,
+                          [{ip, Addr}, {port, Port}],
+                          [{compress, true}, {env, [{dispatch, Dispatch}]}]),
+    wrked_sup:start_link().
+
+stop(_State) ->
+    ok.
