5c43ce0e62036d5133b61a775e3b48ddd109a299
Anton Yabchinskiy <arn@bestmx.net>
2016-02-12 15:19:10 +0300

    web: REST isn't needed in wrked_handler

--- src/wrked_handler.erl
+++ src/wrked_handler.erl
@@ -3,17 +3,10 @@
 %% Common handler callbacks
 -export([init/2]).
 
-%% REST handler callbacks
--export([content_types_provided/2]).
-
 %%%===================================================================
 %%% Common handler callbacks
 %%%===================================================================
 
-init(Req, Opts) -> {cowboy_rest, Req, Opts}.
-
-%%%===================================================================
-%%% REST handler callbacks
-%%%===================================================================
-
-content_types_provided(Req, State) -> {_Result = [], Req, State}.
+init(Req, _Opts) ->
+    %% TODO
+    {ok, Req, _State = undefined}.
