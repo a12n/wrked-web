ff1d5aa89b26dcaf814cb3efb62316542817f41d
Anton Yabchinskiy <arn@bestmx.net>
2016-02-14 01:25:50 +0300

    web: return undefined from wrk2fit() instead of raising the error
    Gives port_close() a change to actually close the port.

--- src/wrked_handler.erl
+++ src/wrked_handler.erl
@@ -28,12 +28,7 @@ wrk2fit(_Name, _Sport, Wrk) ->
     Path = application:get_env(wrked, wrk2fit_path, "/usr/local/bin/wrk2fit"),
     Port = open_port({spawn_executable, Path}, [binary, stream, use_stdio]),
     port_command(Port, [Wrk, <<"EOF">>]),
-    Reply =
-        receive
-            {Port, {data, Data}} ->
-                Data
-        after 1000 ->
-                error(timeout)
-        end,
+    Fit = receive {Port, {data, Data}} -> Data;
+          after 1000 -> undefined end,
     port_close(Port),
-    Reply.
+    Fit.
